180879d3ef74707f5953207d062989178b2f22d2
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-10 17:28:45 -0400

    setting up Add button and AddEntryViewController

---
+++ MemoryJournal/AddEntryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddEntryViewController.swift
+//  MemoryJournal
+//
+//  Created by DetroitLabs on 9/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class AddEntryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
