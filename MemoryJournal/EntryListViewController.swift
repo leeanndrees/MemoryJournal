fe0bdf301c9fdf5f6ecc18abc579ebdceaad094e
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-10 16:38:31 -0400

    displaying journal entry titles in cells

--- MemoryJournal/EntryListViewController.swift
+++ MemoryJournal/EntryListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  EntryListViewController.swift
 //  MemoryJournal
 //
 //  Created by DetroitLabs on 9/10/18.
@@ -18,9 +18,12 @@
 import UIKit
 
 class EntryListViewController: UITableViewController {
+    
+    var journalEntriesToShow = JournalEntryList().journalEntriesToShow
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(journalEntriesToShow)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,7 +31,16 @@ class EntryListViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return journalEntriesToShow.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JournalEntryOverview", for: indexPath)
+        cell.textLabel?.text = journalEntriesToShow[indexPath.row].entryTitle
+        return cell
+    }
 
 }
 
---
+++ MemoryJournal/JournalEntryList.swift
@@ -0,0 +1,19 @@
+//
+//  JournalEntryList.swift
+//  MemoryJournal
+//
+//  Created by DetroitLabs on 9/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+class JournalEntryList {
+    
+    var journalEntriesToShow = [
+        JournalEntry(entryTitle: "Hello", entryContent: "This is a journal entry", entryDate: "September 10, 2018"),
+        JournalEntry(entryTitle: "Sup", entryContent: "Look, another journal entry", entryDate: "September 5, 2018"),
+        JournalEntry(entryTitle: "Hi again", entryContent: "Yep, a journal entry!", entryDate: "September 1, 2018"),
+    ]
+    
+}
