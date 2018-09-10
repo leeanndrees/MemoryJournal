09fb27b36944702f7570301c1ac8983d9804e5cf
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-10 16:45:23 -0400

    setting up detail view controller and nav controller

---
+++ MemoryJournal/EntryDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EntryDetailViewController.swift
+//  MemoryJournal
+//
+//  Created by DetroitLabs on 9/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+   // }
+
+}
--- MemoryJournal/EntryListViewController.swift
+++ MemoryJournal/EntryListViewController.swift
@@ -23,7 +23,6 @@ class EntryListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(journalEntriesToShow)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
