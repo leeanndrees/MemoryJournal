9d2332f55f4670f7810927f83d16c9788d72c86a
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-10 16:48:20 -0400

    setting up basic segue, and an enum to hold Segue Identifiers

---
+++ MemoryJournal/SegueIdentifier.swift
@@ -0,0 +1,13 @@
+//
+//  SegueIdentifier.swift
+//  MemoryJournal
+//
+//  Created by DetroitLabs on 9/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+import Foundation
+
+enum SegueIdentifier: String {
+    case showDetail = "ShowEntryDetail"
+}
