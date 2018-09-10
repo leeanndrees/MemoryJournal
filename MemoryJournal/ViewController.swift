a6071645d326b64fded9b0cefdedc6a1cb42e55b
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-10 15:31:51 -0400

    initial commit

---
+++ MemoryJournal/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MemoryJournal
+//
+//  Created by DetroitLabs on 9/10/18.
+//  Copyright © 2018 DetroitLabs. All rights reserved.
+//
+
+/*
+ Build an app for a user to keep a Memory Journal
+ Your app’s main page should feature a list of existing journal entries by title
+ When you click on a title, you should be taken to the journal entry
+ Journal entries should have a title, content, and a date
+ The user should be able to view, add, edit, and delete journal entries
+ Your app should utilize the delegate pattern to pass data
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
