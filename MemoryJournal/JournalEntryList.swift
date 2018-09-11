27b627159c86771305746aad7adb488e95f2ca1a
Leeann Drees <leeann.drees@detroitlabs.com>
2018-09-11 18:37:00 -0400

    fun text (from cupcake ipsum) for entries, plus some autolayout nonsense w/ edit button

--- MemoryJournal/JournalEntryList.swift
+++ MemoryJournal/JournalEntryList.swift
@@ -11,9 +11,9 @@ import Foundation
 class JournalEntryList {
     
     var journalEntriesToShow = [
-        JournalEntry(entryTitle: "Hello", entryContent: "This is a journal entry", entryDate: "September 10, 2018"),
-        JournalEntry(entryTitle: "Sup", entryContent: "Look, another journal entry", entryDate: "September 5, 2018"),
-        JournalEntry(entryTitle: "Hi again", entryContent: "Yep, a journal entry!", entryDate: "September 1, 2018"),
+        JournalEntry(entryTitle: "Bonbon Dessert Danish", entryContent: "Cupcake ipsum dolor sit amet apple pie jelly beans. Jujubes sweet jelly jelly beans bonbon dessert danish. Gummi bears cookie marshmallow cotton candy pie sugar plum apple pie halvah. Pie ice cream oat cake jelly beans. Gummi bears sugar plum biscuit sweet roll gummi bears powder fruitcake toffee cake.", entryDate: "September 10, 2018"),
+        JournalEntry(entryTitle: "Lemon Drops & Sesame Snaps", entryContent: "Powder jelly-o bear claw oat cake wafer sesame snaps powder bear claw. Gummies cake donut fruitcake. Candy canes apple pie wafer. Chupa chups pie tart cake bear claw sweet roll pie. Caramels cotton candy muffin apple pie caramels croissant sweet cupcake.", entryDate: "September 5, 2018"),
+        JournalEntry(entryTitle: "Tiramisu soufflé", entryContent: "Tiramisu sesame snaps dragée. Lollipop soufflé powder chocolate cake marzipan topping fruitcake. Tootsie roll oat cake croissant gummies caramels jelly-o biscuit. Croissant pastry marzipan cake sesame snaps lollipop tart lollipop.", entryDate: "September 1, 2018"),
     ]
     
 }
